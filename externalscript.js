5092b183e7e6c6fff75d785ae19d48e6a1dd7f65
Alys <alice.g.cooper@gmail.com>
2022-04-12 17:02:42 -0400

    Add beginning scores

--- externalscript.js
+++ externalscript.js
@@ -1,39 +1,38 @@
-          //javascript goes here!
-          console.log("Hello, world!")
-
 // this will be in a five round game that keeps score over time - 
 
-// this function will report a winner or loser at the end          
+// this function will report a winner or loser at the end   
+// we will start with both sides at 0
+let playerScore = 0;
+let computerScore = 0;       
 // The computer will have a choice of three random variables (rock paper scissors), called computerPlay
 // 0 is rock, 1 is paper, 2 is scissors
 
 
 // The computer will need to randomly choose bw those three variables as well and that value stored as "computerSelection"
-
-function computerPlay() {
-    const arrayRPS = ["rock","paper","scissors"];
-    const computerSelection = arrayRPS [Math.floor (Math.random () * arrayRPS.length)]
-    return computerSelection;
+//    const arrayRPS = ["rock","paper","scissors"]; - moving to just rock to make it easier
+function computerSelection() {
+    const arrayRPS = ["rock"];
+    const computerChoice = arrayRPS [Math.floor (Math.random () * arrayRPS.length)]
+    return computerChoice;
 }
 
 // Player input to select between three variables, rock paper and scissors, and save var as "playerSelection"
-const playerSelection = prompt("Please make your play: rock, paper, or scissors!");
+let playerSelection = prompt("Please make your play: rock, paper, or scissors!");
+console.log(playerSelection);
 
-
-// The game will be played (computerPlay) that will set playerSelection against computerSelection and return a string that declares the winner of the round like "You Lose! Paper beats Rock"
-// each round should be the playRound function
+// The game will be played (playRound) that will set playerSelection against computerSelection and return a string that declares the winner of the round like "You Lose! Paper beats Rock"
 //for some reason everything is now "it's a tie"...
 function playRound(playerSelection, computerSelection) {
-  if (computerSelection === playerSelection) {
-      alert("It's a tie!");
-  } else if (computerSelection == "rock" &&  playerSelection == "scissors") {
-      alert("You lose!");
-  } else if (computerSelection == "paper" &&  playerSelection == "rock") {
-     alert("You lose!");
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-     alert("You lose!");
+  if ((playerSelection === "rock") && (computerSelection.toLowerCase() === "scissors")) {
+    console.log("You lose!");
+  } else if (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") {
+    console.log("You lose!");
+  } else if (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper") {
+    console.log("You lose!");
+  } else if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
   } else {
-     alert("You won!!!");
+    console.log("You won!!!");
   }
 }
 
