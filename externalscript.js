77581aee05e7d0602cf92cebf4750bdd622bc0e4
Alys <alice.g.cooper@gmail.com>
2022-04-13 11:14:32 -0400

    cleaning up functionality

--- externalscript.js
+++ externalscript.js
@@ -15,13 +15,15 @@ function computerPlay() {
 }
 
 
-//prompt player to choose their selection
-// let playerSelection = prompt("Choose your fighter: rock, paper, or scissors!")
-
 // the game function will nest the play Round for 5 times
 function game () {
+//start game with both scores at Zero
+  let playerScore = 0;
+  let computerScore = 0;  
+// five rounds
   for (let i = 0; i < 5; i++) {
-      function playRound(playerSelection, computerSelection) {
+// conditions for win lose tie
+    function playRound(playerSelection, computerSelection) {
       if (playerSelection === "rock" && computerSelection === "rock") {
         return("tie");
       } else if (playerSelection === "rock" && computerSelection === "paper") {
@@ -42,7 +44,9 @@ function game () {
         return("tie");
       } 
     } 
+     //prompt player to choose their selection
     let playerSelection = prompt("Choose your fighter: rock, paper, or scissors!")
+    //set computerPlay() value stored as "computerSelection"
     const computerSelection = computerPlay();
     console.log(playRound(playerSelection, computerSelection));
 
@@ -50,48 +54,38 @@ function game () {
     switch (playRound(playerSelection, computerSelection)) {
       case "win":
         alert("Congrats, you won!");
+        playerScore++;
+        alert("Your score is now ${playerScore} and the Computer's score is now ${computerScore}.");
         break;
       case "tie":
         alert("It's a tie!");
+        alert("Your score is now ${playerScore} and the Computer's score is now ${computerScore}.");
         break;
       case "lose":
         alert("Boo, you lost!");
+        computerScore++;
+        alert("Your score is now ${playerScore} and the Computer's score is now ${computerScore}.");
         break;
       default:
         alert("oops");
         }
+
+//    if playerScore
   }
 }  
 
 // The game will be played (playRound) that will set playerSelection against computerSelection and return a string that declares the winner of the round like "You Lose! Paper beats Rock"
-//function playRound(playerSelection, computerSelection) {
-  //  if (playerSelection === "rock" && computerSelection === "rock") {
-    //  return("tie");
-    //} else if (playerSelection === "rock" && computerSelection === "paper") {
-    //  return("lose");
-    //} else if (playerSelection === "rock" && computerSelection === "scissors") {
-    //  return("win");
-    //} else if (playerSelection === "paper" && computerSelection === "rock") {
-  //  //  return("win");
-  //  } else if (playerSelection === "paper" && computerSelection === "paper") {
-  //    return("tie");
-  //  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-  //    return("lose");
-  //  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-  //    return("lose");
-  //  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-  //    return("win");
-  //  } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-  //    return("tie");
-  //  } 
-  //} 
 
 
-/* we will start with both sides at 0
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;   
+// we will start with both sides at 0
+
   
-  for (let i = 0; i < 1; i++) { */
+// we will need to report a winner in the end
+// this can be comparative
+// if playerScore > computerScore, alert Player winner
+//   else if computerScore > playerScore, alert Computer winner
+//   else alert tie (it's unlikely but not impossible) 
+  
+
   
 
